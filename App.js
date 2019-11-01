002cb15cd637bc26f72bc4ceed649a5b3b8c1507
henrycaron <57197754+henrycaron@users.noreply.github.com>
2019-11-01 17:01:30 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import * as FileSystem from 'expo-file-system';
 
 import {
     StyleSheet,
     View,
     Text,
-    TouchableOpacity,
-    Image,
+    SafeAreaView, 
+    ScrollView,
+    Linking,
     Button
   } from 'react-native';
 
@@ -18,211 +18,113 @@ import {
   } from '@expo/vector-icons';
   
 import * as Permissions from 'expo-permissions';
-import { Camera } from 'expo-camera';
 
+export default class OutputScreen extends React.Component {
 
-
-
-export default class CameraScreen extends React.Component {
-  state = {
-    hasCameraPermission: null,
-    type: Camera.Constants.Type.back,
-  };
-
-  async componentWillMount() {
-    const { status } = await Permissions.askAsync(Permissions.CAMERA);
-    this.setState({ 
-        hasCameraPermission: status === 'granted',
-        photoURI: '',        
-    });
-  }
-
-  componentDidMount() {
-    FileSystem.makeDirectoryAsync(FileSystem.documentDirectory + 'photos').catch(e => {
-      console.log(e, 'Directory exists');
-    });
-  }
-  render() {
-    const { hasCameraPermission, photoURI } = this.state;
-    
-    console.log('photoURI', photoURI)
-    if (photoURI !==''){
-        return(
-            <View style={styles.container}>
-                <Image
-                  style={styles.pillimage}
-                  source={{ uri: this.state.photoURI}}
-                />
-                <Text style={styles.title}> Your Image </Text>
-                <View style = {styles.OutputButtonContainer}>
-                   <TouchableOpacity
-                        onPress={() => this.setState({ photoURI: ''})}
-                        style={{ alignSelf: 'center' }}
-                        >
-                        <Octicons name="arrow-left" size={70} color="black" />
-                    </TouchableOpacity>
-                     
-                    <TouchableOpacity
-                        onPress={() => this.setState({ DrugScreen: 'true'})}
-                        style={{ alignSelf: 'center' }}
-                        >
-                        <Octicons name="check" size={70} color="black" />
-                    </TouchableOpacity>
-                </View>
+    render() {
+        return (
+            <View style={ styles.container}>
+                <Text style={styles.titleText}>Ibuprofen</Text>
+                <SafeAreaView style={styles.container}>
+                        <Text style ={styles.subtitleText}> Description </Text>
+                            <Text style ={styles.bodyText}> Nonsteroidal anti-inflammatory drug </Text>
+                        <Text style ={styles.subtitleText}> Alternative Brand Names </Text>
+                            <Text style ={styles.bodyText}>  Advil, 
+                             NeoProfen, Caldolor, and more </Text>
+                    <ScrollView style={styles.scrollView}>
+                        <Text style ={styles.scrollSubtitleText}> Common Uses </Text>
+                            <Text style ={styles.scrollBodyText}> Ibuprofen is used to 
+                                reduce fever and treat pain or inflammation caused 
+                                by many conditions such as headache, toothache, 
+                                back pain, arthritis, menstrual cramps, or minor 
+                                injury. </Text>
+                        <Text style ={styles.scrollSubtitleText}> Side Effects </Text>
+                        <Text style ={styles.scrollBodyText}> Upset stomach, mild heartburn, nausea, vomiting, bloating, 
+                        gas, diarrhea, constipation, dizziness, headache, 
+                        nervousness, decreased appetite, mild itching or rash; or 
+                        ringing in your ears. </Text>
+                        <Text style ={styles.scrollSubtitleText}> How to Use </Text>
+                            <Text style ={styles.scrollBodyText}>
+                                Take this medication by mouth, usually every 4 
+                                to 6 hours with a full glass of water (8 ounces/
+                                240 milliliters) unless your doctor directs you 
+                                otherwise. Do not lie down for at least 10 minutes 
+                                after taking this drug. If you have stomach upset 
+                                while taking this medication, take it with food, 
+                                milk, or an antacid.
+                                </Text>
+                        <Text style = {styles.scrollSubtitleText}> Find out More</Text>
+                        <Button
+                            title="Visit WebMD"
+                            onPress={this._handleOpenWithLinking}
+                            style={styles.button}
+                            />
+                        
+                    </ScrollView>
+                </SafeAreaView>
             </View>
-        
-        )
+
+        );
+      }
     }
     
-    else if (hasCameraPermission === null) {
-      return <View />;
-    } else if (hasCameraPermission === false) {
-      return <Text>No access to camera</Text>;
-    } else {
-      return (
-        <View style={{ flex: 1 }}>
-          <Camera style={{ flex: 1 }} type={this.state.type}
-          ref={ref => {
-            this.camera = ref;
-            }}>
-            <View
-              style={{
-                flex: 8,
-                backgroundColor: 'transparent',
-                flexDirection: 'column',
-              }}>
-              <View style = {{flex: 0.5}, {justifyContent: "center"}}>
-              <TouchableOpacity
-                style={{
-                  flex: 0.3,
-                  alignSelf: 'flex-start',
-                  alignItems: 'center',
-                }}
-                onPress={() => {
-                  this.setState({
-                    type:
-                      this.state.type === Camera.Constants.Type.back
-                        ? Camera.Constants.Type.front
-                        : Camera.Constants.Type.back,
-                  });
-                }}>
-                <Ionicons name="ios-swap" size={40} color="white" />
-                </TouchableOpacity>
-                </View>
-                <View style={styles.CircleShapecontainer}>
-                    <View style={styles.CircleShapeView}></View>
-                    <Text style={styles.title}> Capture capsule within guide </Text>
-                </View>
-            </View>
-            <View style={{ flex: 2}}>
-                <TouchableOpacity
-                    onPress={this.snap}
-                    style={{ alignSelf: 'center' }}
-                >
-                <Ionicons name="ios-radio-button-on" size={70} color="white" />
-              </TouchableOpacity>
-            </View>
-          </Camera>
-        </View>
-      );
-    }
-  }
-
-  
-  snap = async () => {
-    console.log("Snap Image")
-    if (this.camera) {
-        console.log("In This Camera")
-    
-        let photo = await this.camera.takePictureAsync({
-          quality:0.5,
-          skipProcessing:true
-      });
-      console.log("Printed Image", photo.uri)
-      this.setState({
-        photoURI: photo.uri
-      })
-    }
-  };
+_handleOpenWithLinking = () => {
+    Linking.openURL('https://expo.io');
 }
-
-
+      
+    
+    
 const styles = StyleSheet.create({
     container: {
-        alignSelf: 'center',
-        marginTop: 5,
-    },
-    title: {
-        alignSelf: 'center',
-        color: 'white',
-        textShadowColor:'#585858',
-        textShadowOffset:{width: 0, height: 0},
-        textShadowRadius:10,
-    },
-    fixToText: {
-      flexDirection: 'row',
-      justifyContent: 'space-between'
-    },
-    separator: {
-      marginVertical: 8,
-      borderBottomColor: '#737373',
-      borderBottomWidth: StyleSheet.hairlineWidth
-    },
-    captureButtonSection: {
-      width: '100%',
-      height: '30%',
-      justifyContent: 'center',
-      alignItems: 'center'
-    },
-    CircleShapecontainer: {
         flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'center',
+        justifyContent: 'center', 
         alignItems: 'center',
-    },
-    CircleShapeView: {
-        width: 250,
-        height: 250,
-        borderRadius: 50,
-        borderWidth: 2,
-        borderColor: 'white'
-    },   
-     backButton: {
-        flex: 0.3, 
-        height: 60, 
-        justifyContent: 'center',
-        alignItems: 'flex-start',
-    },
-      okayButton: {
-        flex: 0.3, 
-        height: 60, 
-        justifyContent: 'center',
-        alignItems: 'flex-end',
-    },
-      bottomBar: {
-        backgroundColor: 'transparent',
-        alignSelf: 'center',
-        justifyContent: 'space-between',
-        flex: 0.12,
-        flexDirection: 'row',
-    },
-    pillimage: {
-        width: 300, 
-        height: 300,
-        borderRadius: 50,
-        borderWidth: 2,
-        borderColor: 'grey',
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        
-    },
-    OutputButtonContainer: {
-        flex: .5,
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        alignItems: 'center',
-    },
+        backgroundColor: '#708090',
 
-  });
+    },
+    titleText: {
+        fontWeight: 'bold',
+        fontSize: 36, 
+        color: 'white',
+        marginTop: 50,
+        textAlign: 'center'
+    },
+    subtitleText: {
+        fontWeight: 'bold',
+        fontSize: 18, 
+        color: 'white',
+        textAlign: 'center'
+    },
+    bodyText: {
+        fontWeight: 'normal',
+        fontSize: 14,
+        color: '#F5F5F5',
+        justifyContent: 'center',
+        textAlign: 'center'
+    },
+    scrollSubtitleText: {
+        fontWeight: '200',
+        fontSize: 18, 
+        color: '#708090',
+        textAlign: 'center',
+        marginHorizontal: 20,
+        marginTop: 10,
+    },
+    scrollBodyText: {
+        fontWeight: '100',
+        fontSize: 14,
+        color: '#708090',
+        justifyContent: 'center',
+        textAlign: 'center',
+        marginHorizontal: 20,
+    },
+    scrollView: {
+        backgroundColor: 'white',
+        marginHorizontal: 50,
+      },
+    button: {
+        marginVertical: 10,
+        marginHorizontal: 50
+    
+    }
+});
